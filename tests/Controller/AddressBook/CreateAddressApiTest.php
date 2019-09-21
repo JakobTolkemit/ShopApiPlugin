abfa4abfb26e2d37cd6fd05092dd02293d228172
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2019-09-21 16:09:20 +0200

    Trying to use generated client in api test

--- tests/Controller/AddressBook/CreateAddressApiTest.php
+++ tests/Controller/AddressBook/CreateAddressApiTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Sylius\ShopApiPlugin\Controller\AddressBook;
 
 use PHPUnit\Framework\Assert;
+use Swagger\Client\Api\AddressApi;
+use Swagger\Client\ApiException;
+use Swagger\Client\Model\LoggedInCustomerAddressBookAddress;
 use Sylius\Component\Core\Model\AddressInterface;
 use Sylius\Component\Core\Model\Customer;
 use Sylius\Component\Core\Repository\AddressRepositoryInterface;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
 use Tests\Sylius\ShopApiPlugin\Controller\Utils\ShopUserLoginTrait;
 
@@ -22,25 +24,24 @@ final class CreateAddressApiTest extends JsonApiTestCase
      */
     public function it_allows_user_to_add_new_address_to_address_book(): void
     {
+        $addressClient = new AddressApi();
+
         $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'country.yml']);
         $this->logInUser('oliver@queen.com', '123password');
+        $addressClient->getConfig()->setApiKey('Authorization', $this->client->getServerParameter('HTTP_Authorization'));
 
-        $data =
-<<<JSON
-        {
-            "firstName": "New name",
-            "lastName": "New lastName",
-            "phoneNumber": "0918972132",
-            "countryCode": "GB",
-            "provinceCode": "GB-WLS",
-            "street": "New street",
-            "city": "New city",
-            "postcode": "2000"
-        }
-JSON;
+        $data = new LoggedInCustomerAddressBookAddress([
+            "first_name" => "New name",
+            "last_name" => "New lastName",
+            "phone_number" => "0918972132",
+            "country_code" => "GB",
+            "province_code" => "GB-WLS",
+            "street" => "New street",
+            "city" => "New city",
+            "postcode" => "2000",
+        ]);
 
-        $response = $this->createAddress($data);
-        $this->assertResponse($response, 'address_book/add_address_response', Response::HTTP_CREATED);
+        $addressClient->createAddress($data);
 
         /** @var CustomerRepositoryInterface $customerRepository */
         $customerRepository = $this->get('sylius.repository.customer');
@@ -62,24 +63,24 @@ JSON;
      */
     public function it_does_not_allow_user_to_add_new_address_to_address_book_without_passing_required_data(): void
     {
+        $addressClient = new AddressApi();
+
         $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'country.yml']);
         $this->logInUser('oliver@queen.com', '123password');
+        $addressClient->getConfig()->setApiKey('Authorization', $this->client->getServerParameter('HTTP_Authorization'));
 
-        $data =
-<<<JSON
-        {
-            "firstName": "",
-            "lastName": "",
-            "countryCode": "",
-            "street": "",
-            "city": "",
-            "countryCode": "",
-            "postcode": ""
-        }
-JSON;
+        $data = new LoggedInCustomerAddressBookAddress([
 
-        $response = $this->createAddress($data);
-        $this->assertResponse($response, 'address_book/validation_create_address_book_response', Response::HTTP_BAD_REQUEST);
+            "first_name" => "",
+            "last_name" => "",
+            "country_code" => "",
+            "street" => "",
+            "city" => "",
+            "postcode" => "",
+        ]);
+
+        $this->expectException(ApiException::class);
+        $addressClient->createAddress($data);
     }
 
     /**
@@ -87,23 +88,24 @@ JSON;
      */
     public function it_does_not_allow_user_to_add_new_address_to_address_book_without_passing_correct_country_code(): void
     {
+        $addressClient = new AddressApi();
+
         $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'country.yml']);
         $this->logInUser('oliver@queen.com', '123password');
+        $addressClient->getConfig()->setApiKey('Authorization', $this->client->getServerParameter('HTTP_Authorization'));
 
-        $data =
-<<<JSON
-        {
-            "firstName": "Davor",
-            "lastName": "Duhovic",
-            "countryCode": "WRONG_COUNTRY_NAME",
-            "street": "Marmontova 21",
-            "city": "Split",
-            "postcode": "2100"
-        }
-JSON;
+        $data = new LoggedInCustomerAddressBookAddress([
 
-        $response = $this->createAddress($data);
-        $this->assertResponse($response, 'address_book/validation_create_address_book_with_wrong_country_response', Response::HTTP_BAD_REQUEST);
+            "first_name" => "Davor",
+            "last_name" => "Duhovic",
+            "country_code" => "WRONG_COUNTRY_NAME",
+            "street" => "Marmontova 21",
+            "city" => "Split",
+            "postcode" => "2100",
+        ]);
+
+        $this->expectException(ApiException::class);
+        $addressClient->createAddress($data);
     }
 
     /**
@@ -111,30 +113,24 @@ JSON;
      */
     public function it_does_not_allow_user_to_add_new_address_to_address_book_without_passing_correct_province_code(): void
     {
+        $addressClient = new AddressApi();
+
         $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'country.yml']);
         $this->logInUser('oliver@queen.com', '123password');
+        $addressClient->getConfig()->setApiKey('Authorization', $this->client->getServerParameter('HTTP_Authorization'));
 
-        $data =
-<<<JSON
-        {
-            "firstName": "Davor",
-            "lastName": "Duhovic",
-            "countryCode": "GB",
-            "street": "Marmontova 21",
-            "city": "Split",
-            "postcode": "2100",
-            "provinceCode": "WRONG_PROVINCE_CODE"
-        }
-JSON;
+        $data = new LoggedInCustomerAddressBookAddress([
 
-        $response = $this->createAddress($data);
-        $this->assertResponse($response, 'address_book/validation_create_address_book_with_wrong_province_response', Response::HTTP_BAD_REQUEST);
-    }
+            "first_name" => "Davor",
+            "last_name" => "Duhovic",
+            "country_code" => "GB",
+            "street" => "Marmontova 21",
+            "city" => "Split",
+            "postcode" => "2100",
+            "province_code" => "WRONG_PROVINCE_CODE",
+        ]);
 
-    private function createAddress(string $data): Response
-    {
-        $this->client->request('POST', '/shop-api/address-book/', [], [], self::CONTENT_TYPE_HEADER, $data);
-
-        return $this->client->getResponse();
+        $this->expectException(ApiException::class);
+        $addressClient->createAddress($data);
     }
 }
