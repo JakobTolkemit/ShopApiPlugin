78e459814ce1316fc43086d8f40152bc54285579
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2019-09-22 10:55:31 +0200

    Trying to get the tests green

--- tests/Controller/JsonApiTestCase.php
+++ tests/Controller/JsonApiTestCase.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Tests\Sylius\ShopApiPlugin\Controller;
 
+use Swagger\Client\Api\AddressApi;
+use Swagger\Client\Api\CartApi;
+use Swagger\Client\Api\CheckoutApi;
+use Swagger\Client\Api\OrderApi;
+use Swagger\Client\Api\ProductsApi;
+use Swagger\Client\Api\TaxonsApi;
+use Swagger\Client\Api\UsersApi;
+use Swagger\Client\Configuration;
+
 abstract class JsonApiTestCase extends \ApiTestCase\JsonApiTestCase
 {
     public const CONTENT_TYPE_HEADER = ['CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'];
@@ -22,5 +31,48 @@ abstract class JsonApiTestCase extends \ApiTestCase\JsonApiTestCase
         }
 
         return parent::get($id);
+    }
+
+    protected function getAddressClient(): AddressApi
+    {
+        return new AddressApi(null, $this->getClientConfig());
+    }
+
+    protected function getCartClient(): CartApi
+    {
+        return new CartApi(null, $this->getClientConfig());
+    }
+
+    protected function getCheckoutClient(): CheckoutApi
+    {
+        return new CheckoutApi(null, $this->getClientConfig());
+    }
+
+    protected function getUsersClient(): UsersApi
+    {
+        return new UsersApi(null, $this->getClientConfig());
+    }
+
+    protected function getOrderClient(): OrderApi
+    {
+        return new OrderApi(null, $this->getClientConfig());
+    }
+
+    protected function getProductsClient(): ProductsApi
+    {
+        return new ProductsApi(null, $this->getClientConfig());
+    }
+
+    protected function getTaxonsClient(): TaxonsApi
+    {
+        return new TaxonsApi(null, $this->getClientConfig());
+    }
+
+    private function getClientConfig(): Configuration
+    {
+        $config = new Configuration();
+        $config->setHost('localhost:8080');
+
+        return $config;
     }
 }
